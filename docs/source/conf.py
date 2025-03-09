4d9144812d961754eeac489fb6b5d8e1507dbcb2
vesamy1234 <vesamy123@gmail.com>
2025-03-09 11:03:03 +0700

    Add files via upload

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,12 +13,7 @@ author = 'Ve Samy'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme', 
-              'sphinx_copybutton', 
-              'sphinx_new_tab_link', 
-              'sphinxemoji.sphinxemoji',
-              'sphinx_favicon',
-              ]
+extensions = ['sphinx_rtd_theme', 'sphinx_copybutton',  'sphinx_new_tab_link', 'sphinxemoji.sphinxemoji','sphinx_favicon']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -28,7 +23,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = ['alabaster', ]
+html_theme = 'alabaster'
 
 html_static_path = ['_static']
 def setup(app):
